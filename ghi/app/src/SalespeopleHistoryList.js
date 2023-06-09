b71f9d77499b5ea87527a93a92dc9001e45ef60b
Alexander Levero <Alexander.Levero@gmail.com>
2023-06-09 21:27:18 +0300

    Completed and tested Salesperson History functionality

--- ghi/app/src/SalespeopleHistoryList.js
+++ ghi/app/src/SalespeopleHistoryList.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 function SalespeopleHistoryList() {
     const [salespeople, setSalespeople] = useState([])
     const [sales, setSales] = useState([])
+    const [query, setQuery] = useState("")
     const fetchdata = async () => {
         const url = 'http://localhost:8090/api/salespeople/';
         const response = await fetch(url);
@@ -16,7 +17,7 @@ function SalespeopleHistoryList() {
         const sales_url = 'http://localhost:8090/api/sales';
         const sales_response = await fetch(sales_url);
         if (sales_response.ok) {
-            const data = await response.json();
+            const data = await sales_response.json();
             setSales(data.sales);
         }
     }
@@ -24,16 +25,15 @@ function SalespeopleHistoryList() {
     useEffect(() => {
         fetchdata();
     }, []);
-
     return (
         <>
             <h1>Salesperson History</h1>
             <div className="mb-3">
-                <select required name="salesperson" id="salesperson" className="form-select">
+                <select value={query} onChange={e => setQuery(e.target.value)} required name="salesperson" id="salesperson" className="form-select">
                     <option value="">Choose a salesperson</option>
                     {salespeople.map(salesperson => {
                     return (
-                        <option key={salesperson.id} value={salesperson.id}>{salesperson.first_name + " " + salesperson.last_name}</option>
+                        <option key={salesperson.id} value={salesperson.first_name }>{salesperson.first_name + " " + salesperson.last_name}</option>
                     )
                     })}
                 </select>
@@ -48,12 +48,12 @@ function SalespeopleHistoryList() {
                     </tr>
                 </thead>
                 <tbody>
-                    {sales.map(sale => {
+                    {sales.filter(sale=> sale.salesperson.first_name === query) .map(sale => {
                         return (
                             <tr key={sale.id}>
-                                <td>{ sale.salesperson }</td>
-                                <td>{ sale.customer }</td>
-                                <td>{ sale.vin }</td>
+                                <td>{ sale.salesperson.first_name } { sale.salesperson.last_name }</td>
+                                <td>{ sale.customer.first_name } { sale.customer.last_name }</td>
+                                <td>{ sale.automobile.vin }</td>
                                 <td>{ sale.price }</td>
                             </tr>
                         );
--- sales/api/sales_rest/admin.py
+++ sales/api/sales_rest/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import AutomobileVO, Salesperson, Customer, Sale
 
-# Register your models here.
+
+admin.site.register(AutomobileVO)
+admin.site.register(Salesperson)
+admin.site.register(Customer)
+admin.site.register(Sale)
